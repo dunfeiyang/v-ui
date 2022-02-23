db2821af919fe287ce995283475ea722972a50eb
dunfeiyang <dfylk@qq.com>
2022-02-24 00:17:03 +0800

    dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>dialog</div>
+  <div>示例1</div>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="x" :ok="f1" :cancel="f2"></Dialog>
 </template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Button from "../lib/Button.vue";
+import Dialog from "../lib/Dialog.vue";
+
+export default {
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
+};
+</script>
